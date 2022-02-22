fa4e302c45640110b607bd49cce03f5d78b584d4
Park J.S <sylvan0212@gmail.com>
2022-02-22 21:11:04 +0900

    prepare to apply repository

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -9,45 +9,40 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { BoardStatus } from './board-status.enum';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
-  constructor(private boardsService: BoardsService) {}
-
-  @Get('/')
-  getAllBoards(): Board[] {
-    // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
-    return this.boardsService.getAllBoards();
-  }
-
-  @Post('/')
-  @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
-    return this.boardsService.createBoard(createBoardDto);
-  }
-
-  @Get('/:id')
-  getBoardById(@Param('id') id: string): Board {
-    // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
-    return this.boardsService.getBoardById(id);
-  }
-
-  @Delete('/:id')
-  deleteBoardById(@Param('id') id: string): void {
-    this.boardsService.deleteBoard(id);
-  }
-
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
-  ): Board {
-    // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
-    return this.boardsService.updateBoardStatus(id, status);
-  }
+  // constructor(private boardsService: BoardsService) {}
+  // @Get('/')
+  // getAllBoards(): Board[] {
+  //   // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
+  //   return this.boardsService.getAllBoards();
+  // }
+  // @Post('/')
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
+  //   return this.boardsService.createBoard(createBoardDto);
+  // }
+  // @Get('/:id')
+  // getBoardById(@Param('id') id: string): Board {
+  //   // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
+  //   return this.boardsService.getBoardById(id);
+  // }
+  // @Delete('/:id')
+  // deleteBoardById(@Param('id') id: string): void {
+  //   this.boardsService.deleteBoard(id);
+  // }
+  // @Patch('/:id/status')
+  // updateBoardStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  // ): Board {
+  //   // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
+  //   return this.boardsService.updateBoardStatus(id, status);
+  // }
 }
