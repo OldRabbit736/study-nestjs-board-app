9702dd29d38407306b77d4abf96b408b9c8b0785
Park J.S <sylvan0212@gmail.com>
2022-02-22 21:25:06 +0900

    added getBoardById in controller and service

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -10,13 +10,15 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
-  // constructor(private boardsService: BoardsService) {}
+  constructor(private boardsService: BoardsService) {}
+
   // @Get('/')
   // getAllBoards(): Board[] {
   //   // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
@@ -28,6 +30,12 @@ export class BoardsController {
   //   // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
   //   return this.boardsService.createBoard(createBoardDto);
   // }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: number): Promise<Board> {
+    return this.boardsService.getBoardById(id);
+  }
+
   // @Get('/:id')
   // getBoardById(@Param('id') id: string): Board {
   //   // 엔티티 그대로 리턴하면 안 좋다. 리턴 타입을 새로 만드는 게 좋다.
