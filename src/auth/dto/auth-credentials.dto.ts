016b319c23e702e1e811c94f6c3188dc5415d59c
OldRabbit736 <sylvan0212@gmail.com>
2022-03-04 21:22:14 +0900

    added validation to AuthCredentialsDto

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(2)
+  @MaxLength(10)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  // 영어, 숫자만 가능하도록
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'password only accepts alphabets and numbers',
+  })
   password: string;
 }
