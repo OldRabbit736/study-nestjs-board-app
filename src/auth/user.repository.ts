3be402a39e2b4d108113be6f2b093e52956f73b0
OldRabbit736 <sylvan0212@gmail.com>
2022-03-04 22:31:47 +0900

    username contrained to be uniuqe

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -11,6 +15,14 @@ export class UserRepository extends Repository<User> {
       password,
     });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
